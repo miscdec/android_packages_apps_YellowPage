b680c387e168a4059f6d2d0e606bc42c4731376f
miscdec <70028744+miscdec@users.noreply.github.com>
2024-02-03 19:12:58 +0800

    test：add a test for online number lookup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compiler.get()
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
---
+++ app/src/androidTest/java/org/exthmui/yellowpage/ExampleInstrumentedTest.kt
@@ -0,0 +1,56 @@
+package org.exthmui.yellowpage
+
+import android.content.Context
+import android.util.Log
+import androidx.annotation.NonNull
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.exthmui.yellowpage.lookup.BaiduLookup
+import org.exthmui.yellowpage.lookup.Safe360Lookup
+import org.exthmui.yellowpage.lookup.SogouLookup
+import org.exthmui.yellowpage.misc.Constants
+import org.exthmui.yellowpage.models.PhoneNumberInfo
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    private val phoneNumberLookups = arrayOf(SogouLookup(), BaiduLookup(), Safe360Lookup())
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.exthmui.yellowpage", appContext.packageName)
+
+        lookupInfoOnline(appContext, "10000", 86).apply {
+            Log.d("testlookup-final", this.toString())
+        }
+    }
+
+    private fun lookupInfoOnline(
+        context: Context,
+        @NonNull number: String,
+        countryCode: Long
+    ): PhoneNumberInfo? {
+        var info: PhoneNumberInfo? = null
+        for (phoneNumberLookup in phoneNumberLookups) {
+            if (countryCode == 0L || phoneNumberLookup.checkRegion(countryCode)) {
+                info = phoneNumberLookup.lookup(context, number)
+                Log.d("testlookup", info.toString())
+                if (info.type != Constants.PhoneNumberTagData.TYPE_NORMAL) {
+                    break
+                }
+            }
+        }
+        return info
+    }
+}
+
